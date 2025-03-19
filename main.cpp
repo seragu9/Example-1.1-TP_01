c9fab97db23165b6e5512db714267b9d60bccb90
seragu9 <seragu9@gmail.com>
2025-03-19 23:54:51 +0000

    cambio

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
-#include "mbed.h"
+/*! @mainpage Example 1.1 Guia 1/01
+ * @date Miercoles, Marzo 19, 2025
+ * @authors Sebastian Andrada , Sergio Aguirre
+ * @section genDesc General Description
+ *
+ * Aprendiendo a comentar con doxygen
+ *
+ * @section genRem General Remarks
+ * Aca tengo un programa que hace uso de un stm32
+ *
+ */
+//=====[Libraries]=============================================================
+#include "mbed.h" 
 #include "arm_book_lib.h"
-#include <cstdio>
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+//m
+//=====[Main function, the program entry point after power on or reset]========
+
+/**
+* Declara imputs y outputs, inicializa variables.
+* @param none
+* @return el valor devuelto 
+*/
 
 int main()
 {
-    DigitalIn gasDetector(D2);
+    SystemClock_Config();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    //gpio_read(&gpio)
+    DigitalIn gasDetector(D2); // declara gasDetector como entrada en D2
+    
+    DigitalOut alarmLed(LED1); // declara alarmLed como salida en LED1
 
-    DigitalOut alarmLed(LED1);
+    gasDetector.mode(PullDown); // Activa el pull Down para D2
 
-    gasDetector.mode(PullDown);
+    alarmLed = OFF;  //Inicializo, alamrLed inicialmente apagado
 
-    alarmLed = OFF;
     printf("%s\n", "Hello World");
-    while\n (true) {
+    //int i;
+    //i = 0;
+
+    while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
         }
@@ -20,5 +51,56 @@ int main()
         if ( gasDetector == OFF ) {
             alarmLed = OFF;
         }
+        printf("%s\n","hola mundo");
     }
+}
+
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
